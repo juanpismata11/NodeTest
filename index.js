dbd44dcff7dc195492504a4df47826087095ff18
Juan Pablo <juanpismata11@gmail.com>
2025-05-29 18:22:08 -0600

    Segundo commit y agregando lo faltantt

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("Hello, Git Fundamentals!");
+console.log("This is a basic setup for Git Fundamentals on macOS.");
 
+console.log("To get started, make sure you have Git installed on your system.");
+console.log("agregando faltante")
